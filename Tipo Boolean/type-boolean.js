98037831f7a885513c042ebe03a2846b5317220b
Lucas Theodoro <112442170+LucasTheodoroSilva@users.noreply.github.com>
2023-07-25 11:59:49 -0300

    Add files via upload

---
+++ Tipo Boolean/type-boolean.js
@@ -0,0 +1,21 @@
+// tipo Boolean
+// true - verdadeiro
+// false - falso
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero); //false
+
+
+const terceiroNumero = 5;
+const quartoNumero = 5;
+
+console.log(terceiroNumero === quartoNumero); //true
+
+const cadastroAtivo = true;
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2); //false
